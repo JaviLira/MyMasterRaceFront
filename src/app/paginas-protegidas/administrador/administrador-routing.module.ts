581e233edff3b7d6436ce602b6ed1c14d092246c
Javi Lira <javilirasanchez20011812@gmail.com>
2022-06-05 23:13:24 +0200

    modulo no utilizado borrado

--- src/app/paginas-protegidas/administrador/administrador-routing.module.ts
+++ src/app/paginas-protegidas/administrador/administrador-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'crearOrdenador', loadChildren: () => import('./crear-ordenador/crear-ordenador.module').then(m => m.CrearOrdenadorModule) },
   { path: 'crearComponente', loadChildren: () => import('./crear-componente/crear-componente.module').then(m => m.CrearComponenteModule) },
   { path: 'editar-articulo', loadChildren: () => import('./editar-articulos/editar-articulos.module').then(m => m.EditarArticulosModule) },
   { path: 'listUsers', loadChildren: () => import('./list-user/list-user.module').then(m => m.ListUserModule) }
