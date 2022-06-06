05440c7a62666b472676d4fa75ee9ab2ae7740aa
macabros554 <macabros554@gmail.com>
2022-06-06 23:27:40 +0200

    cambiando mensajes de error lista de usuarios

--- src/app/paginas-protegidas/administrador/list-user/pedidos-user/pedidos-user.component.ts
+++ src/app/paginas-protegidas/administrador/list-user/pedidos-user/pedidos-user.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { PedidosUsuarioService } from '../../services/pedidos-user-componente.service';
 
 @Component({
   selector: 'app-pedidos-user',
@@ -8,9 +9,10 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class PedidosUserComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute,private pedidoUserService:PedidosUsuarioService) { }
 
   ngOnInit(): void {
+
   }
 
   idUsuario:string=this.route.snapshot.paramMap.get('id')!;
